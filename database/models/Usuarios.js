2114f5f25f440523c2b3d10f4144a05308034362
gitcore33 <erickestanislau25@gmail.com>
2023-03-20 00:12:23 -0300

    inserindo página de adm e iniciando configuração de relacionamentos

--- database/models/Usuarios.js
+++ database/models/Usuarios.js
@@ -1,18 +1,27 @@
-module.exports = (sequelize, DataType) => {
+const Compras = require('./Compras')
+
+module.exports = (sequelize, DataTypes) => {
     const Usuarios = sequelize.define('Usuarios', {
         nome: {
-            type:DataType.STRING(100),
+            type:DataTypes.STRING(100),
             allowNull: true
         },
-        data_nascimento: DataType.DATE,
-        telefone: DataType.INTEGER,
-        cpf: DataType.INTEGER,
-        sexo: DataType.INTEGER,
-        email: DataType.STRING(45),
-        avaliacao_loja: DataType.INTEGER
+        data_nascimento: DataTypes.DATE,
+        telefone: DataTypes.INTEGER,
+        cpf: DataTypes.INTEGER,
+        sexo: DataTypes.INTEGER,
+        email: DataTypes.STRING(45),
+        avaliacao_loja: DataTypes.INTEGER
     }, {
         tableName: 'usuarios'
     })
+
+    Usuarios.associate = models => {
+        Usuarios.hasMany(models.Compras, {
+            foreignKey: 'usuarios_id',
+            as: 'usuarios'
+        })
+    }
 
         return Usuarios
 }
